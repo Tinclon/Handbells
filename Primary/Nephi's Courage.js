cf60b063b73aa660f1cb94e251e893cd80d17c88
Nielsen, Clinton <clinton_nielsen@intuit.com>
2023-01-15 14:29:41 -0700

    feat: add Nephi's Courage

---
+++ Primary/Nephi's Courage.js
@@ -0,0 +1,226 @@
+var Nephi_s_Courage = [
+    {
+        "Harmony": [
+            // The
+            {"n": "r", "d": 1 / 8},
+
+            // Lord commanded Nephi
+            {"n": "c", "d": 1 / 2},
+            {"n": "d", "d": 1 / 2},
+
+            // To go and get the plates
+            {"n": "a# low", "d": 1 / 4},
+            {"n": "a# low", "d": 1 / 4},
+            {"n": "a low", "d": 1 / 2},
+
+            // From the wicked Laban
+            {"n": "c", "d": 1 / 2},
+            {"n": "c#", "d": 1 / 2},
+
+            // Inside the city gates
+            {"n": "a# low", "d": 1 / 4},
+            {"n": "a# low", "d": 1 / 4},
+            {"n": "r", "d": 1 / 2},
+
+            // Laman and Lemuel
+            {"n": "g#", "d": 1 / 2},
+            {"n": "f", "d": 1 / 2},
+
+            // were both afraid to try
+            {"n": "f", "d": 1 / 2},
+            {"n": "e", "d": 1 / 2},
+
+            // Nephi was courageous
+            {"n": "c", "d": 1 / 2},
+            {"n": "d", "d": 1 / 2},
+
+            // This was his reply
+            {"n": "a# low", "d": 1 / 4},
+            {"n": "a# low", "d": 1 / 4},
+            {"n": "a low", "d": 1 / 4 + 1 / 8},
+
+            // I will go I will do
+            {"n": "r", "d": 1 / 2},
+            {"n": "r", "d": 1 / 4},
+            {"n": "f", "d": 1 / 4},
+            {"n": "e", "d": 1 / 4},
+
+            // The thing the Lord commands
+            {"n": "c", "d": 1 / 2},
+            {"n": "f", "d": 1 / 4},
+            {"n": "e", "d": 1 / 4},
+
+            // I know the Lord provides a way
+            {"n": "f", "d": 1 / 4},
+            {"n": "f", "d": 1 / 4},
+            {"n": "e", "d": 1 / 4},
+            {"n": "c", "d": 1 / 4},
+
+            // He wants be to obey
+            {"n": "f", "d": 1 / 2},
+            {"n": "c", "d": 1 / 4},
+
+            // I will go I will do
+            {"n": "r", "d": 1 / 2},
+            {"n": "r", "d": 1 / 4},
+            {"n": "f", "d": 1 / 4},
+            {"n": "e", "d": 1 / 4},
+
+            // The thing the Lord commands
+            {"n": "c", "d": 1 / 2},
+            {"n": "f", "d": 1 / 4},
+            {"n": "e", "d": 1 / 4},
+
+            // I know the Lord provides a way
+            {"n": "f", "d": 1 / 4},
+            {"n": "f", "d": 1 / 4},
+            {"n": "e", "d": 1 / 4},
+            {"n": "c", "d": 1 / 4},
+
+            // He wants be to obey
+            {"n": "a# low", "d": 1 / 4},
+            {"n": "a# low", "d": 1 / 4},
+            {"n": "a low", "d": 1 / 4 + 1 / 4},
+
+        ]
+    },
+    {
+        "Melody": [
+
+            // The
+            {"n": "c", "d": 1 / 8},
+
+            // Lord commanded Nephi
+            {"n": "c", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "a", "d": 1 / 4},
+            {"n": "f", "d": 1 / 8},
+
+            // To go and get the plates
+            {"n": "f", "d": 1 / 8},
+            {"n": "d", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "e", "d": 1 / 8},
+            {"n": "f", "d": 1 / 2},
+
+            // From the wicked Laban
+            {"n": "c", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "g#", "d": 1 / 4},
+            {"n": "f", "d": 1 / 8},
+
+            // Inside the city gates
+            {"n": "f", "d": 1 / 8},
+            {"n": "c#", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "e", "d": 1 / 8},
+            {"n": "f", "d": 1 / 2},
+
+            // Laman and Lemuel
+            {"n": "c high", "d": 1 / 4},
+            {"n": "c# high", "d": 1 / 8},
+            {"n": "c# high", "d": 1 / 8},
+            {"n": "c high", "d": 1 / 4},
+            {"n": "g#", "d": 1 / 8},
+
+            // were both afraid to try
+            {"n": "g#", "d": 1 / 8},
+            {"n": "a#", "d": 1 / 8},
+            {"n": "c high", "d": 1 / 8},
+            {"n": "a#", "d": 1 / 8},
+            {"n": "g#", "d": 1 / 8},
+            {"n": "g", "d": 1 / 2},
+
+            // Nephi was courageous
+            {"n": "c", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "a", "d": 1 / 4},
+            {"n": "f", "d": 1 / 4},
+
+            // This was his reply
+            {"n": "d", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "e", "d": 1 / 8},
+            {"n": "f", "d": 1 / 4 + 1 / 8},
+
+            // I will go I will do
+            {"n": "c", "d": 1 / 8},
+            {"n": "c", "d": 1 / 8},
+            {"n": "f", "d": 1 / 4},
+            {"n": "c", "d": 1 / 8},
+            {"n": "c", "d": 1 / 8},
+            {"n": "c high", "d": 1 / 4 + 1 / 8},
+
+            // The thing the Lord commands
+            {"n": "a#", "d": 1 / 8},
+            {"n": "a", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "a", "d": 1 / 8},
+            {"n": "a#", "d": 1 / 8},
+            {"n": "g", "d": 1 / 4 + 1 / 8},
+
+            // I know the Lord provides a way
+            {"n": "c high", "d": 1 / 8},
+            {"n": "d high", "d": 1 / 8},
+            {"n": "a#", "d": 1 / 8},
+            {"n": "c high", "d": 1 / 8},
+            {"n": "a", "d": 1 / 8},
+            {"n": "a#", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "a", "d": 1 / 8},
+
+            // He wants be to obey
+            {"n": "f", "d": 1 / 8},
+            {"n": "d high", "d": 1 / 8},
+            {"n": "c high", "d": 1 / 8},
+            {"n": "a#", "d": 1 / 8},
+            {"n": "a", "d": 1 / 8},
+            {"n": "g", "d": 1 / 4},
+
+            // I will go I will do
+            {"n": "c", "d": 1 / 8},
+            {"n": "c", "d": 1 / 8},
+            {"n": "f", "d": 1 / 4},
+            {"n": "c", "d": 1 / 8},
+            {"n": "c", "d": 1 / 8},
+            {"n": "c high", "d": 1 / 4 + 1 / 8},
+
+            // The thing the Lord commands
+            {"n": "a#", "d": 1 / 8},
+            {"n": "a", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "a", "d": 1 / 8},
+            {"n": "a#", "d": 1 / 8},
+            {"n": "g", "d": 1 / 4 + 1 / 8},
+
+            // I know the Lord provides a way
+            {"n": "c high", "d": 1 / 8},
+            {"n": "d high", "d": 1 / 8},
+            {"n": "a#", "d": 1 / 8},
+            {"n": "c high", "d": 1 / 8},
+            {"n": "a", "d": 1 / 8},
+            {"n": "a#", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "a", "d": 1 / 8},
+
+            // He wants be to obey
+            {"n": "f", "d": 1 / 8},
+            {"n": "d", "d": 1 / 8},
+            {"n": "g", "d": 1 / 8},
+            {"n": "f", "d": 1 / 8},
+            {"n": "e", "d": 1 / 8},
+            {"n": "f", "d": 1 / 4 + 1 / 4},
+        ]
+    },
+];
+
+Song("Nephi's Courage (120)", Nephi_s_Courage, 100, 4);
